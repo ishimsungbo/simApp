b7584a1bafd911088d9e2fa910803ef7a2ec41da
sungbo <ishimsungbo@gmail.com>
2022-09-26 16:06:14 +0900

    jpa 간단한 테스트케이스까지 설정/완료

--- src/main/java/com/sim/app/AppController.java
+++ src/main/java/com/sim/app/AppController.java
@@ -4,19 +4,43 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.MessageSource;
 
+import java.util.Locale;
+
+@Slf4j
 @Api(tags = {"1. Index and Test"})  //스웨거
 @RequiredArgsConstructor
 @RestController
 public class AppController {
 
+    @Autowired
+    MessageSource messageSource;
+
     @GetMapping("/")
     public String index(){
         return "<h2>SimApp Spring boot Home Page</h2>";
     }
+
+    @GetMapping("/message01")
+    public String message01(){
+        Locale.setDefault(Locale.ROOT);
+
+        String[] str = new String[]{"i18n"};
+
+        System.out.println(messageSource.getMessage("greeting", str, Locale.getDefault()));
+        System.out.println(messageSource.getMessage("greeting", str, Locale.US));
+        System.out.println(messageSource.getMessage("greeting", str, Locale.KOREA));
+
+        return "메세지 세팅 확인 중";
+    }
+
 
     @ApiOperation(value = "테스트 value" , notes = "테스트 notes")  //스웨거 어노테이션
     @GetMapping("/Test/{name}")
---
+++ src/main/java/com/sim/app/config/MessageConfig.java
@@ -0,0 +1,21 @@
+package com.sim.app.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+//@Configuration
+public class MessageConfig {
+   /*
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("message");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(3);
+
+        return messageSource;
+    }
+    */
+}
