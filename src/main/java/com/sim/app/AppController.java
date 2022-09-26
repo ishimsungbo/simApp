d1d958d208165e058da0f0007a2dc6800ff4459b
sungbo <ishimsungbo@gmail.com>
2022-09-26 10:43:38 +0900

    스웨거2 로 설정 완료

--- src/main/java/com/sim/app/AppController.java
+++ src/main/java/com/sim/app/AppController.java
@@ -1,8 +1,13 @@
 package com.sim.app;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = {"1. Index and Test"})  //스웨거
+@RequiredArgsConstructor
 @RestController
 public class AppController {
 
@@ -11,4 +16,9 @@ public class AppController {
         return "<h2>SimApp Spring boot Home Page</h2>";
     }
 
+    @ApiOperation(value = "테스트 value" , notes = "테스트 notes")  //스웨거 어노테이션
+    @GetMapping("/Test")
+    public String Test(){
+        return "안녕하세요";
+    }
 }
--- src/main/java/com/sim/app/config/SwaggerApiConfig.java
+++ src/main/java/com/sim/app/config/SwaggerApiConfig.java
@@ -10,16 +10,16 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@Configuration
-//@EnableSwagger2
+@Configuration
+@EnableSwagger2
 public class SwaggerApiConfig {
-    /**
+
     @Bean
     public Docket swaggerApi(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(swaggerInfo())  // API Docu 및 작성자 정보 매핑
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.sim.app.restapi.user"))
+                .apis(RequestHandlerSelectors.basePackage("com.sim.app"))
                 .paths(PathSelectors.any()) //Controller 패키지 전부?
                 .build()
                 .useDefaultResponseMessages(false);
@@ -33,5 +33,5 @@ public class SwaggerApiConfig {
                 .version("0.1")
                 .build();
     }
-    **/
+
 }
