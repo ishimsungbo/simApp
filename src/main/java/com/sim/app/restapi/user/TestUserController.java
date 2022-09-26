1e01e4416fc6402b1c741b87492fa5edff36dcfe
sungbo <ishimsungbo@gmail.com>
2022-09-26 11:48:33 +0900

    jpa 간단한 테스트케이스까지 설정/완료

--- src/main/java/com/sim/app/restapi/user/TestUserController.java
+++ src/main/java/com/sim/app/restapi/user/TestUserController.java
@@ -1,25 +1,31 @@
 package com.sim.app.restapi.user;
 
+import org.hibernate.query.criteria.internal.expression.function.CurrentDateFunction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 public class TestUserController {
 
-
+    @Autowired
     private UserJpaRepo userJpaRepo;
 
     @PostMapping("/user/saveUser")
-    public User testSaveUser(){
+    public String testSaveUser(){
 
         User user = User.builder()
-                .email("abcfile@naver.com")
-                .name("고짱구")
+                .email(LocalDateTime.now()+"@xxx.com")
+                .name("uName "+ LocalDateTime.now())
                 .build();
 
-        return userJpaRepo.save(user);
+        userJpaRepo.save(user);
+
+        return "성공";
     }
 
     @GetMapping("/user/findUser/{testUserName}")
