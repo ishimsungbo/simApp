5f0ece4f67532f8bdf3b458504cd7ca7593bffdb
sungbo <ishimsungbo@gmail.com>
2022-09-26 17:04:47 +0900

    jpa 간단한 테스트케이스까지 설정/완료

--- src/main/java/com/sim/app/AppController.java
+++ src/main/java/com/sim/app/AppController.java
@@ -6,7 +6,9 @@ import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.MessageSource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +25,9 @@ public class AppController {
     @Autowired
     MessageSource messageSource;
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @GetMapping("/")
     public String index(){
         return "<h2>SimApp Spring boot Home Page</h2>";
@@ -32,14 +37,33 @@ public class AppController {
     public String message01(){
         Locale.setDefault(Locale.ROOT);
 
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/message_resource/message");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(3);
+        messageSource.clearCache();
+        messageSource.setConcurrentRefresh(true);
+
         String[] str = new String[]{"i18n"};
 
         System.out.println(messageSource.getMessage("greeting", str, Locale.getDefault()));
         System.out.println(messageSource.getMessage("greeting", str, Locale.US));
         System.out.println(messageSource.getMessage("greeting", str, Locale.KOREA));
 
+
         return "메세지 세팅 확인 중";
     }
+
+    @GetMapping("/beanList")
+    public String[] beanList(){
+        String[] beans = applicationContext.getBeanDefinitionNames();
+
+        for (String bean : beans) {
+            System.out.println("bean : " + bean);
+        }
+        return beans;
+    }
+
 
 
     @ApiOperation(value = "테스트 value" , notes = "테스트 notes")  //스웨거 어노테이션
--- src/main/java/com/sim/app/config/MessageConfig.java
+++ src/main/java/com/sim/app/config/MessageConfig.java
@@ -1,21 +1,43 @@
 package com.sim.app.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
-//@Configuration
+import java.util.Locale;
+
+@Configuration
 public class MessageConfig {
-   /*
+
+    @Bean
+    public LocaleResolver defaultLocaleResolver() {
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREAN);
+
+        return localeResolver;
+    }
+
     @Bean
     public MessageSource messageSource() {
+
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("message");
+        messageSource.setBasename("classpath:/message_resource/message");
         messageSource.setDefaultEncoding("UTF-8");
         messageSource.setCacheSeconds(3);
 
+        Locale.setDefault(Locale.KOREAN); // 제공하지 않는 언어로 들어왔을 때 처리
+
         return messageSource;
     }
-    */
+
+    @Bean
+    public MessageSourceAccessor messageSourceAccessor (
+            @Autowired ReloadableResourceBundleMessageSource messageSource) {
+        return new MessageSourceAccessor(messageSource);
+    }
 }
